323f83055b6f31e692eb97ffbac2231967fe6add
Johnny Vaz <johnny.ferreira@luizalabs.com>
2023-10-10 05:50:44 -0300

    pdf ppa

--- data/livroData.ts
+++ data/livroData.ts
@@ -3,7 +3,7 @@ const livroData = [
     title: 'Método PPA - Prosseguindo para o Alvo',
     description: `Clique aqui e adquira o E-Book Sistema de Aprendizagem Baseado em Trilha`,
     imgSrc: '/static/images/link-livro.png',
-    href: 'https://checkout.xgrow.com/143a620f-2faf-4129-80b6-44b27752b33a/MTg0MjE=',
+    href: '/static/Livro_PPA-Prosseguindo-para-o-Alvo.pdf',
   }
 ]
 
